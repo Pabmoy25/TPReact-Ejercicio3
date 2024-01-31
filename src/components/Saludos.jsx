a262c18d9814d77cd937d34fd144b604aea54813
Pabmoy25 <pabmoy1725@gmail.com>
2024-01-31 17:49:32 -0300

    se creo un boton para modificar el componente Saludos usando State

--- src/components/Saludos.jsx
+++ src/components/Saludos.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 const Saludos = (props) => {
+
+    const [modificaSaludo, setModificaSaludo] = useState("");
     return (
         <section>
-            <h2>Hello {props.enviarProps} </h2>
+            <h2>Hello {props.enviarProps} {modificaSaludo} </h2>
+            <button
+        className="btn btn-primary"
+        onClick={() => setModificaSaludo("(From changed State)!")}
+      >
+        Click me
+      </button>
         </section>
     );
 };
