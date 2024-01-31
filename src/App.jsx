976a0e151ddb0bb9ec634d9b7a50190db9b124a5
Pabmoy25 <pabmoy1725@gmail.com>
2024-01-31 17:39:50 -0300

    se crea el componente Footer y se aplica maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
 import Saludos from "./components/Saludos";
+import Footer from "./components/Footer";
 
 function App() {
  
@@ -13,6 +14,7 @@ function App() {
     
     
     </section>
+    <Footer></Footer>
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <section className="bg-dark text-light py-3 text-center">
+            <p>&copy; Todos los derechos reservados</p>
+        </section>
+    );
+};
+
+export default Footer;
